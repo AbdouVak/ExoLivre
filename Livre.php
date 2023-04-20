3c5d96a9ecb50e7003d7cb43b21c1bc23ddc533d
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-20 11:20:32 +0200

    Class Livre

--- Livre.php
+++ Livre.php
@@ -5,16 +5,18 @@ class Livre{
     private string $_nbPage;
     private string $_anneeParution;
     private string $_prix;
+    private Auteur $_auteur;
 
 
-    function __construct( string $titre, string $anneeParution, string $nbPage, string $prix){
+    function __construct( string $titre, string $anneeParution, string $nbPage, string $prix, Auteur $auteur){
         $this->_titre =$titre;
         $this->_nbPage = $nbPage;
         $this->_anneeParution = $anneeParution;
         $this->_prix = $prix;
-
+        $this->_auteur = $auteur;
+        $this->_auteur->addBook($this);
     }
-
+    
     public function __toString()
     {
         return "$this->_titre ($this->_anneeParution) : $this->_nbPage pages / $this->_prix €<br>";
